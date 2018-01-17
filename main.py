0c15172ebda330cabced775d0fccfa6220771b85
Nathan <naleba97@gmail.com>
2018-01-17 16:53:52 -0600

    Add selenium firefox web driver

--- main.py
+++ main.py
@@ -5,22 +5,28 @@ import re
 import json
 import urllib2
 import requests
+import time
+from selenium import webdriver
+import selenium.common.exceptions
 from urllib import urlencode
 
 reddit = None
 subreddit = None
 posts_replied_to = None
+driver = None
 
 def setup():
-	global reddit, subreddit, posts_replied_to
+	global reddit, subreddit, posts_replied_to, driver
 
-	reddit = praw.Reddit('fs_bot');
-	subreddit = reddit.subreddit("testingground4bots");
+	reddit = praw.Reddit('fs_bot')
+	subreddit = reddit.subreddit("testingground4bots")
+	driver = webdriver.Firefox()
 
 
 
 def search():
-	global posts_replied_to
+	global posts_replied_to, driver
+	driver = webdriver.Firefox()
 	'''
 	print("Listening for posts from amazon.com")
 	try:
@@ -48,28 +54,46 @@ def search():
 	'''
 
 	#Test code
-	url="https://www.amazon.com/Creativity-Kids-Grow-Glow-Terrarium/dp/B00I9KDFK0/ref=pd_bxgy_21_img_3?_encoding=UTF8&pd_rd_i=B00I9KDFK0&pd_rd_r=ZHGGDTNC69H473QA2HX3&pd_rd_w=byV4x&pd_rd_wg=af1pw&psc=1&refRID=ZHGGDTNC69H473QA2HX3"
+	url = "https://www.amazon.com/Surprise-Outrageous-Littles-Sisters-Friends/dp/B0755K9DJ8/ref=pd_bxgy_21_img_3?_encoding=UTF8&pd_rd_i=B0755K9DJ8&pd_rd_r=XEPVNYBT9VZRWQPJEVWM&pd_rd_w=YGtkS&pd_rd_wg=2LVUt&psc=1&refRID=XEPVNYBT9VZRWQPJEVWM"
 
 	fakespot_url = "https://www.fakespot.com/analyze/?"
 
 	params = {'url' : url}
 
-	req = requests.get(fakespot_url + urlencode(params)) 
+	full_url = fakespot_url + urlencode(params)
+
+	driver.get(full_url)
+
+	print(driver.current_url)
 
 	try:
-		page = urllib2.urlopen(req.url)
-		
-		html = page.read()
+		result = check_reanalyze(driver.page_source, driver.current_url, driver)
 
-		if check_reanalyze(page, html):
-			html = page.read()
+		if result:
+			try:
+				while driver.find_element_by_class_name("analysis-status"):
+					time.sleep(5)	
+					pass
 
-		grade = get_grade(html)
+				grade = get_grade(driver.page_source)
 
-		total_reviews = get_total_reviews(html)
+				total_reviews = get_total_reviews(driver.page_source)
 
-		print(total_reviews)
-		print(grade)
+				print(total_reviews)
+			
+				print(grade)
+
+			except selenium.common.exceptions.ElementNotSelectableException
+		else:
+			html = driver.page_source
+			grade = get_grade(html)
+
+			total_reviews = get_total_reviews(html)
+
+			print(total_reviews)
+			print(grade)
+
+		driver.close()
 		
 		
 
@@ -79,14 +103,14 @@ def search():
 '''
 Checks whether the currently displayed analysis is old. If so, make a GET request to reanalyze the site.
 '''
-def check_reanalyze(page, html):
-	fakespot_url = "https://www.fakespot.com/reanalyze/"
+def check_reanalyze(html, url, driver):
 	if re.search("This analysis is quite old", html):
+		fakespot_url = "https://www.fakespot.com/reanalyze/"
 		reg = re.search("/reanalyze/([0-9A-Za-z]*)\"", html)
 		fakespot_url = fakespot_url + reg.group(1)
 		req = requests.get(fakespot_url)
-		while re.search("reanalyze",page.read()):
-			pass
+		driver.get(url)
+		time.sleep(5)
 		return True
 	else:
 		return False
@@ -107,4 +131,5 @@ def print_grade():
 	print()
 
 if __name__ == '__main__':
+	#setup()
 	search()
