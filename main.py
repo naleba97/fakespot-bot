4d5da86d574ec422fcb6dfbabd0653145fd1534e
Nathan <naleba97@gmail.com>
2018-01-18 20:50:47 -0600

    Add more information to reply and improve exception handling

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import json
 import urllib2
 import requests
 import time
+import datetime
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.firefox.options import Options
@@ -20,7 +21,7 @@ def setup():
 	global reddit, subreddit, posts_replied_to, driver
 
 	reddit = praw.Reddit('fs_bot')
-	subreddit = reddit.subreddit("testingground4bots")
+	subreddit = reddit.subreddit("buildapcsales")
 
 	options = Options()
 	options.add_argument("--headless")
@@ -40,28 +41,33 @@ def run():
 	print("Listening for the command !fs_bot...")
 	try:
 		for comment in subreddit.stream.comments():
+			print(datetime.datetime.fromtimestamp(comment.created))
 			if comment.submission.id not in posts_replied_to:
 				submission = comment.submission
 				if re.search("!fs_bot", comment.body, re.IGNORECASE):
 					if re.search("amazon.com", submission.url, re.IGNORECASE):
 						reply = search(submission.url)
-						print("Printing reply... \n" + reply)
+						print("Generating reply... \n" + reply)
 						
 						comment.reply(reply)
 
 						posts_replied_to.append(submission.id)
-						mark_post_as_replied(submission.id)
-
-					
+						mark_post_as_replied(submission.id)	
 	except prawcore.exceptions.RequestException:
 		print("Reddit API has a problem. Restarting bot...")
 		run()
 	except praw.exceptions.APIException as e:
 		print(e.message + "\n")
-		reg = re.search("try again in ([0-9]*) minutes", e.message)
-		if reg:
-			print("Waiting for " + reg.group(1) + " minutes...\n")
+		reg_sec = re.search("second", e.message)
+		reg_min = re.search("minute", e.message)
+		if reg_min:
+			reg = re.search("try again in ([0-9]*)", e.message)
+			print("Waiting" + reg.group(1) + " minutes...\n")
 			time.sleep(int(reg.group(1))*60 + 60)
+		elif reg_sec:
+			reg = re.search("try again in ([0-9]*)", e.message)
+			print("Waiting" + reg.group(1) + " seconds...\n")
+			time.sleep(int(reg.group(1)) + 60)
 		else:
 			print("Waiting for 10 minutes...\n")
 			time.sleep(600)
@@ -145,9 +151,12 @@ def make_comment(html, url):
 	header = create_header(html)
 	grade = get_grade(html)
 	total_reviews = get_total_reviews(html)
+	amazon_rating = get_amazon_rating(html)
+	company_grade = get_company_grade(html)
 	redirect = get_redirect(url)
+	footer = create_footer()
 
-	return header+grade+total_reviews+redirect
+	return header+grade+total_reviews+amazon_rating+company_grade+redirect+footer
 
 
 def check_reanalyze(html, url, driver):
@@ -170,9 +179,21 @@ def create_header(html):
 	"""
 	Creates the header of the comment and returns it.
 	"""
-
 	reg = re.search("<title>([\w\s|]*)", html)
-	return "**" + reg.group(1) + "**\n\n"
+	if reg:
+		return "**" + reg.group(1) + "**\n\n"
+	else:
+		return "Could not create header.\n\n"
+
+def create_footer():
+	"""
+	Creates the footer of the comment and returns it.
+	Includes link to source code and page to report bugs/errors.
+	"""
+	code_url = "https://github.com/naleba97/fakespot-bot"
+	error_url = "https://github.com/naleba97/fakespot-bot/issues"
+	return "> [Source Code](" + code_url + ") | " + "[Report Errors](" + error_url + ")\n\n" 
+
 
 def get_redirect(url):
 	"""
@@ -184,16 +205,44 @@ def get_total_reviews(html):
 	"""
 	Searches and returns the current total amount of reviews.
 	"""
-
 	reg = re.search("Total Reviews</div><p>([0-9]*)", html)
-	return "Total number of reviews: " + reg.group(1) + "\n\n"
+	if reg:
+		return "Total number of reviews: " + reg.group(1) + "\n\n"
+	else:
+		return "Could not locate total number of reviews.\n\n"
 
 def get_grade(html):
 	"""
 	Searches and returns the current Fakespot grade.
 	"""
 	reg = re.search("Fakespot Grade</div><p>(A|B|C|D|F)",html)
-	return "Fakespot Grade: " + reg.group(1) + "\n\n"
+	if reg:
+		return "Product Fakespot Grade: " + reg.group(1) + "\n\n"
+	else:
+		return "Could not locate Product Fakespot Grade.\n\n"
+
+def get_amazon_rating(html):
+	"""
+	Searches and returns the current Amazon score.
+	"""
+	reg = re.search("rating=\"([\w.]*)\"", html)
+	if reg:
+		return "Amazon Product Star Rating: " + reg.group(1) + "\n\n"
+	else:
+		return "Could not locate Amazon Product Star Rating.\n\n"
+
+
+def get_company_grade(html):
+	"""
+	Searches and returns the company name and grade.
+	"""
+	reg_name = re.search("Sold by[\s&nbsp]*;<[\w\s=\"\-\/]*>([\w\s]*)<", html)
+	reg_grade = re.search("font-grade-(\w+)", html)
+	if reg_name and reg_grade:
+		return "Company Name: " + reg_name.group(1) + "\n\n" + "Company Grade: " + reg_grade.group(1).upper() + "\n\n"
+	else:
+		return "Could not locate Company Name or Grade.\n\n"
+
 
 def mark_post_as_replied(submission_id):
 	"""
